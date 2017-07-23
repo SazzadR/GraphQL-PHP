64d9164397453366376a0d89fd8081aed08e9e21
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2017-07-24 01:34:12 +0600

    LikePost mutation

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ use Blog\Schema\LatestPostField;
 
 use Youshido\GraphQL\Schema\Schema;
 use Youshido\GraphQL\Execution\Processor;
+use Youshido\GraphQL\Type\NonNullType;
 use Youshido\GraphQL\Type\Object\ObjectType;
+use Youshido\GraphQL\Type\Scalar\IntType;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -17,10 +19,30 @@ $rootQueryType = new ObjectType([
     ]
 ]);
 
+$rootMutationType = new ObjectType([
+    'name' => 'RootMutationType',
+    'fields' => [
+        'likePost' => [
+            'type' => new IntType(),
+            'args' => [
+                'id' => new NonNullType(new IntType())
+            ],
+            'resolve' => function () {
+                return 2;
+            }
+        ]
+    ]
+]);
+
 $processor = new Processor(new Schema([
-    'query' => $rootQueryType
+    'query' => $rootQueryType,
+    'mutation' => $rootMutationType
 ]));
 
-$payload = '{ latestPost {title, summary } }';
+/*$payload = '{ latestPost {title, summary } }';
+$processor->processPayload($payload);
+echo json_encode($processor->getResponseData(), true);*/
+
+$payload = 'mutation { likePost(id: 5) }';
 $processor->processPayload($payload);
 echo json_encode($processor->getResponseData(), true);
